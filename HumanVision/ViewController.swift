1e0c0fb35b230926e4bba5802462847eae67e6e6
bunniuni <124115234+bunniuni@users.noreply.github.com>
2023-09-14 17:02:42 -0700

    Initial Commit

---
+++ HumanVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumanVision
+//
+//  Created by Emery Hollingsworth on 9/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
